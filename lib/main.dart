bd5a7f5f453cb1837882d7f223d3bdc1776ecefd
manel kacem <manelkacem11@gmail.com>
2023-11-05 13:55:35 +0100

    column widget basic one ; show children property

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -32,14 +32,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     /// Scaffold; high-level structure for a screen
@@ -49,26 +41,22 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
+
       /// body: the primary content of the scaffold, here we used a container widget
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      /// This example uses a Column to arrange three widgets vertically ⬇,
+      body: const Column(
+        children: <Widget>[
+          Text('Deliver features faster'),
+          Text('Craft beautiful UIs'),
+
+          /// Expanded: a widget that expands a child of a Row, Column, or Flex so that the child fills the available space.
+          Expanded(
+            child: FittedBox(
+              child: FlutterLogo(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
