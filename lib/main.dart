7a29a04e21ab9361d90ab8e446c26752a3993a69
manel kacem <manelkacem11@gmail.com>
2023-11-05 14:24:45 +0100

    row widget demo

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -32,43 +32,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     /// Scaffold; high-level structure for a screen
     /// Provides AppBar and body property as used in this code
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      /// body: the primary content of the scaffold, here we used a container widget
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+
+        /// body: the primary content of the scaffold, here we used a container widget
+        /// 1. See the yellow and black warning stripe
+        /// 2. Add expanded for Text widget which means take only the remaining room not EVERYTHING!
+        /// 3. Use the textDirection to see how it changes the order of children
+        body: const Row(
+          /// Test me out!
+          // textDirection: TextDirection.rtl,
+          children: <Widget>[
+            FlutterLogo(),
+            Text(
+                "Flutter's hot reload helps you quickly and easily experiment, build UIs, add features, and fix bug faster. Experience sub-second reload times, without losing state, on emulators, simulators, and hardware for iOS and Android."),
+            Icon(Icons.sentiment_very_satisfied),
+          ],
+        ));
   }
 }
