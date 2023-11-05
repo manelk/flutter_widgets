9f9971a97bd1857bb7f5ef5047594e8a4dd863cd
manel kacem <manelkacem11@gmail.com>
2023-11-05 21:12:30 +0100

    SafeArea

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+/// SafeArea demo follow the todo and see before and after!
+/// Link: https://www.educative.io/answers/how-to-use-safearea-widget-in-flutter
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,7 +9,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -31,44 +34,15 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+/// TODO : WRAP TEXT WITH SafeArea WIDGET AND SEE THE CHANGES!
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    /// Scaffold; high-level structure for a screen
-    /// Provides AppBar and body property as used in this code
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+    return const Scaffold(
+      body: Text(
+        'Welcome to Educative.io! We hope you are enjoying your time exploring answers and learning with us.',
+        style: TextStyle(fontSize: 22),
       ),
-      /// body: the primary content of the scaffold, here we used a container widget
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
